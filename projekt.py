b7a101faefb3138f28db94cca61651b1b8bcb032
Melker Forsberg <melker.forsberg@learnet.se>
2025-04-02 12:49:56 +0200

    La till några klasser får fixa de senare

--- projekt.py
+++ projekt.py
@@ -1,3 +1,27 @@
-print("test")
+class Cell:
 
-print("TEsta igen")
+    def __init__(self, mina, status, markering, närheten):
+        self.mina= mina 
+        self.status = status
+        self.markering = markering
+        self.närheten = närheten
+
+
+
+class Plan:
+    def __init__(self, bredd, höjd, antal_minor ):
+
+        self.bredd = bredd
+        self.höjd = höjd
+        self.antal_minor = antal_minor
+
+
+
+class Spel:
+    def __init__(self, val, updatera, vinnst, spelplanen):
+        self.val = val
+        self.updatera = updatera
+        self.vinnst = vinnst
+        self.spelplanen = spelplanen
+
+        

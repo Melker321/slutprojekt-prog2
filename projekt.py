3fd4bfeddf04386549b65e5e119d83466803d742
Melker Forsberg <melker.forsberg@learnet.se>
2025-04-29 11:00:37 +0200

    La till pygame(men bara en skärm) för att se hur det fungerar igen. La även till en funktion för att rita ut alla cellen men är inte klar.

--- projekt.py
+++ projekt.py
@@ -1,13 +1,29 @@
+import pygame
+import random
+pygame.init()
+
+nivåer = {
+"lätt" : {"bredd":9, "höjd":9, "minor": 10},
+"medel": {"bredd":14, "höjd":14, "minor":40},
+"svår" : {"bredd":18, "höjd":18, "minor":100} 
+}
+
 class Cell:
-
-
+    def __init__(self, x, y, storlek, mina=False):
         self.x = x
         self.y = y
         self.storlek = storlek
+        self.mina = mina
         self.status = "stängd"
         self.markering = False
         self.närheten = 0
 
+    def rita(self, yta):
+        for x in range (self.bredd):
+            for y in range (self.höjd):
+                cell = self.celler[x][y]
+
+        rect = pygame.Rect(x * cell.storlek, y * cell.storlek, cell.storlek, cell.storlek)
 
 
 class Plan:
@@ -26,4 +42,22 @@ class Spel:
         self.vinnst = vinnst
         self.spelplanen = spelplanen
 
-        
+
+
+fönster = pygame.display.set_mode((640, 480))   
+
+fönster.fill((0,0,0))
+
+plan = Plan(9, 9, 10) 
+plan.rita(fönster) 
+
+
+pygame.display.flip()
+
+while True:
+    for handelse in pygame.event.get():
+        if handelse.type == pygame.QUIT:
+            exit()
+
+
+
